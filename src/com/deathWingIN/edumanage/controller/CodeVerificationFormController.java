b46c34096eb25e197ba42e5d9034d799772271f5
DeathwingIN <imeshnirmal1u@gmail.com>
2024-07-09 17:04:48 +0530

    Rest Password

--- src/com/deathWingIN/edumanage/controller/CodeVerificationFormController.java
+++ src/com/deathWingIN/edumanage/controller/CodeVerificationFormController.java
@@ -2,6 +2,7 @@ package com.deathWingIN.edumanage.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
@@ -18,22 +19,30 @@ public class CodeVerificationFormController {
     public Label txtSelectedEmail;
 
     int code = 0;
+    String selectedEmail = "";
 
     public void setUserData(int verificationCode, String email) {
 
         System.out.println(verificationCode);
         code = verificationCode;
+        selectedEmail = email;
         txtSelectedEmail.setText(email);
     }
 
 
-    public void verifyOnAction(ActionEvent actionEvent) {
+    public void verifyOnAction(ActionEvent actionEvent) throws IOException {
 
 
         if (
                 String.valueOf(code).equals(txtCode.getText())
         ) {
-            //TODO : Change Password
+            FXMLLoader fxmlLoader =
+                    new FXMLLoader(getClass().getResource("../view/ResetPasswordForm.fxml"));
+            Parent parent = fxmlLoader.load();
+            ResetPasswordFormController controller = fxmlLoader.getController();
+            controller.setUserData(selectedEmail);
+            Stage stage = (Stage) context.getScene().getWindow();
+            stage.setScene(new Scene(parent));
         } else {
             new Alert(Alert.AlertType.ERROR, "Invalid Code").show();
         }
--- src/com/deathWingIN/edumanage/controller/ResetPasswordFormController.java
+++ src/com/deathWingIN/edumanage/controller/ResetPasswordFormController.java
@@ -1,8 +1,46 @@
 package com.deathWingIN.edumanage.controller;
 
+import com.deathWingIN.edumanage.db.Database;
+import com.deathWingIN.edumanage.model.User;
+import com.deathWingIN.edumanage.util.security.PasswordManager;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class ResetPasswordFormController {
-    public void changePasswordOnAction(ActionEvent actionEvent) {
+
+    public AnchorPane context;
+    public TextField txtPassword;
+
+    String selectedEmail = "";
+
+    public void setUserData(String email) {
+        selectedEmail = email;
+    }
+
+    public void changePasswordOnAction(ActionEvent actionEvent) throws IOException {
+        Optional<User> selectedUser = Database.UserTable.stream()
+                .filter(e -> e.getEmail().equals(selectedEmail))
+                .findFirst();
+        if (selectedUser.isPresent()) {
+            selectedUser.get().setPassword(new PasswordManager().encrypt(txtPassword.getText()));
+            new Alert(Alert.AlertType.INFORMATION, "Password Changed Successfully").show();
+            setUI("LoginForm");
+        } else {
+            new Alert(Alert.AlertType.ERROR, "User Not Found").show();
+        }
+    }
+
+    private void setUI(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/" + location + ".fxml"))));
+        stage.centerOnScreen();
     }
 }
